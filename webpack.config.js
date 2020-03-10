19636c2705444d51a247643ee8b043dcc18015d2
choisaywhy <sehwa98@icloud.com>
2020-03-10 18:16:50 +0900

    Webpack 개발서버 환경설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,57 @@
 var webpack = require('webpack');
 
 module.exports = {
-    entry: './src/index.js',
+    
+    entry: [
+        './src/index.js',
+        'webpack-dev-server/clients?http://0.0.0.0:4000',
+        'webpack/hot/only-dev-server'
+    ],
 
     output: {
-        path: __dirname + '/public/',
+        path: '/',
         filename: 'bundle.js'
     },
 
     devServer: {
         hot: true,
-        inline: true,
-        host: '0.0.0.0',
-        port: 4000,
-        contentBase: __dirname + '/public/',
+        filename: 'bundle.js',
+        publicPath: '/',
+        historyApiFallback: true,
+        contentBase:'./public/',
+        proxy: {
+            "**" : "http://localhost:3000"
+        },
+        stats: {
+            assets: false,
+            colors: true,
+            version: false,
+            hash: false,
+            timings: false,
+            chunks: false,
+            chunkModules: false
+        }
     },
 
     module: {
         loaders: [
             {
                 test: /\.js$/,
-                loaders: ['react-hot', 'babel?' + JSON.stringify({
-                    cacheDirectory: true,
-                    presets: ['es2015', 'react']
-                })],
+                loader: 'bable',
                 exclude: /node_modules/,
+                query: {
+                    cacheDirecotry: true,
+                    presets: ['es2015', 'react'],
+                    plugins: ["react-hotloader/babel"]
+                }
             }
         ]
     },
 
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        // NoErrosPlugin은 deprecated되었음.
+        new webpack.NoEmitOnErrorsPlugin()
     ]
 };
